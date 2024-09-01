ad3836c617c4f649adb0b89bbb1089d72e0a767c
Noorhesham <noordragon2004@gmail.com>
2024-09-01 16:03:21 +0200

    dr nurse last all responsive

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging.js");
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js');
 
 firebase.initializeApp({
   apiKey: "AIzaSyAp-Tn74eshiw5wCVNfksVFM96_bW0TWOg",
