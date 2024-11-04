bf28ea90b4450910165cc0018475b7244e468eb1
Noorhesham <noordragon2004@gmail.com>
2024-11-04 13:50:28 +0200

    lastly

--- app/[locale]/(home)/(padding)/person/(dashboard)/create-profile/page.tsx
+++ app/[locale]/(home)/(padding)/person/(dashboard)/create-profile/page.tsx
@@ -8,7 +8,7 @@ import React from "react";
 const page = () => {
   const { userSettings, loading } = useAuth();
   if (loading) return <Spinner />;
-  if (userSettings?.has_profile) return redirect("/person/edit-profile");
+  if (userSettings?.has_profile) return redirect("/person/edit-my-profile");
   return (
     <div>
       <ProfileForm />
