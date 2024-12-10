d543db660dc4c0876b2d3f4804b0ab493a329e07
Noorhesham <noordragon2004@gmail.com>
2024-12-10 21:59:18 +0200

    first commit

--- app/[locale]/(home)/hospital-home/page.tsx
+++ app/[locale]/(home)/hospital-home/page.tsx
@@ -28,13 +28,13 @@ export default async function Home() {
         <div className=" w-full  absolute z-[-1] h-screen">
           <Image
             className="object-cover lg:block  hidden w-full h-full"
-            src={page.main_cover_image_for_pc[0].file}
+            src={page.main_cover_image_for_pc?.[0]?.file}
             alt="cover"
             fill
           />
           <Image
             className="object-cover lg:hidden block w-full h-full"
-            src={page.main_cover_image_for_mobile[0]?.file}
+            src={page.main_cover_image_for_mobile?.[0]?.file}
             alt="cover"
             fill
           />
@@ -52,7 +52,7 @@ export default async function Home() {
                   key={i}
                   title={item.number}
                   subTitle={item.important_numbers_title}
-                  icon={item.important_numbers_icon[0].sizes.medium}
+                  icon={item?.important_numbers_icon?.[0]?.sizes?.medium}
                 />
               ))}
             </GridContainer>
@@ -78,75 +78,83 @@ export default async function Home() {
         </div>
         <FlexWrapper className=" h-full">
           <div className="  rounded-[2.5rem] overflow-hidden flex-1 relative aspect-square">
-            <VideoZoom
-              btn={
-                <div className=" cursor-pointer">
-                  <div className="overflow-visible absolute z-10 top-3 right-3">
-                    <PlayIcon />
+            {page?.video_1_youtube_url && (
+              <VideoZoom
+                btn={
+                  <div className=" cursor-pointer">
+                    <div className="overflow-visible absolute z-10 top-3 right-3">
+                      <PlayIcon />
+                    </div>
+                    <Image
+                      alt="image"
+                      fill
+                      className="object-cover "
+                      src={page.introductory_videos_video_1_image?.[0]?.file}
+                    />
                   </div>
-                  <Image
-                    alt="image"
-                    fill
-                    className="object-cover "
-                    src={page.introductory_videos_video_1_image[0].file}
-                  />
-                </div>
-              }
-              content={
-                <div className="relative dynamic-padding w-full h-auto overflow-hidden">
-                  <iframe
-                    className="absolute top-0 left-0 w-full h-full"
-                    src={getYouTubeEmbedUrl(page.video_1_youtube_url)}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    frameBorder="0"
-                  ></iframe>
-                </div>
-              }
-            />
+                }
+                content={
+                  <div className="relative dynamic-padding w-full h-auto overflow-hidden">
+                    <iframe
+                      className="absolute top-0 left-0 w-full h-full"
+                      src={getYouTubeEmbedUrl(page?.video_1_youtube_url)}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      frameBorder="0"
+                    ></iframe>
+                  </div>
+                }
+              />
+            )}
           </div>
           <div className="  rounded-[2.5rem] overflow-hidden flex-1  relative aspect-square">
-            <VideoZoom
-              btn={
-                <div className=" cursor-pointer">
-                  <Image
-                    alt="image"
-                    fill
-                    className="object-cover w-full h-full "
-                    src={page.introductory_videos_video_2_image[0].file}
-                  />
+            {page?.video_2_youtube_url && (
+              <VideoZoom
+                btn={
+                  <div className=" cursor-pointer">
+                    <Image
+                      alt="image"
+                      fill
+                      className="object-cover w-full h-full "
+                      src={page.introductory_videos_video_2_image?.[0]?.file}
+                    />
 
-                  <div className="overflow-visible absolute z-10 top-3 right-3">
-                    <PlayIcon />
+                    <div className="overflow-visible absolute z-10 top-3 right-3">
+                      <PlayIcon />
+                    </div>
                   </div>
-                </div>
-              }
-              content={
-                <div className="relative dynamic-padding w-full h-auto overflow-hidden">
-                  <iframe
-                    className="absolute top-0 left-0 w-full h-full"
-                    src={getYouTubeEmbedUrl(page.video_2_youtube_url)}
-                    title="YouTube video player"
-                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                    frameBorder="0"
-                  ></iframe>
-                </div>
-              }
-            />
+                }
+                content={
+                  <div className="relative dynamic-padding w-full h-auto overflow-hidden">
+                    <iframe
+                      className="absolute top-0 left-0 w-full h-full"
+                      src={getYouTubeEmbedUrl(page.video_2_youtube_url)}
+                      title="YouTube video player"
+                      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                      frameBorder="0"
+                    ></iframe>
+                  </div>
+                }
+              />
+            )}
           </div>
         </FlexWrapper>
       </MaxWidthWrapper>
-      <FindDoctor
-        images={page.about_service_images}
-        title={page.about_service_main_title}
-        miniTitle={page.about_service_small_title}
-        paragraph={page.about_service_content}
-      />
-      <About
-        paragraph={page.our_service_content}
-        services_list={page.services_list}
-        title={page.our_service_main_title}
-      />
+      {page?.about_service_content && (
+        <FindDoctor
+          images={page.about_service_images}
+          title={page.about_service_main_title}
+          miniTitle={page.about_service_small_title}
+          paragraph={page.about_service_content}
+        />
+      )}
+      {page?.our_service_content && (
+        <About
+          paragraph={page.our_service_content}
+          services_list={page.services_list}
+          title={page.our_service_main_title}
+        />
+      )}
       <div className="my-4 lg:my-10 bg-[#F2F5FF]">
         <MaxWidthWrapper>
           <div className=" flex flex-col gap-5 my-4 ">
@@ -187,18 +195,25 @@ export default async function Home() {
       <MaxWidthWrapper>
         <section className="flex  lg:flex-row flex-col items-center gap-5 ">
           <div className=" rounded-2xl flex-1 overflow-hidden aspect-square relative w-full h-full">
-            <Image alt="image" fill className="object-contain " src={page.start_to_end_section_image[0].file} />
+            <Image alt="image" fill className="object-contain " src={page?.start_to_end_section_image?.[0]?.file} />
           </div>
           <div className="flex flex-col items-start gap-10 justify-between  flex-1">
             <div className="flex flex-col items-start">
               <h3 className=" text-main2 text-lg uppercase font-medium my-1">
                 {page.start_to_end_section_small_title}
               </h3>
-              <Head1 alignment="left" animation={false} className=" text-left" text={page.start_to_end_section_main_title} />
-              <div
-                dangerouslySetInnerHTML={{ __html: convertToHTML(page.start_to_end_section_content) }}
-                className={`lg:max-w-2xl text-black text-sm  font-medium my-2 leading-[1.7] `}
+              <Head1
+                alignment="left"
+                animation={false}
+                className=" text-left"
+                text={page.start_to_end_section_main_title}
               />
+              {page?.start_to_end_section_content && (
+                <div
+                  dangerouslySetInnerHTML={{ __html: convertToHTML(page?.start_to_end_section_content) }}
+                  className={`lg:max-w-2xl text-black text-sm  font-medium my-2 leading-[1.7] `}
+                />
+              )}
             </div>
           </div>
         </section>
